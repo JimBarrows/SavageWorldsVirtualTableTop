4eb60aa72f472c3f40d76316d1191bfa97bc94db
Jim Barrows <Jim.Barrows@gmail.com>
2014-08-24 23:45:36 -0700

    Handles build dir being gone.

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,7 @@
-PROJECT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo $PROJECT
+if [ -d "build" ]; then 
+    rm -rf build
+fi
+mkdir build
 sbcl --no-userinit --no-sysinit --non-interactive --load ~/quicklisp/setup.lisp --eval '(ql:write-asdf-manifest-file "build/quicklisp-manifest.txt")'
 
 buildapp --output build/savage-worlds --manifest-file build/quicklisp-manifest.txt --manifest-file ~/quicklisp/local-projects/system-index.txt --asdf-tree . --load-system savage-worlds --entry savage-worlds-api::main
