a4faac781a1b03f7c259d45ff0b47d2d2062d742
Jim Barrows <Jim.Barrows@gmail.com>
2015-01-01 08:19:28 -0700

    No child types, so don't need the inheritance of tables to be specified.

--- repositories/src/main/java/org/savageworlds/game/model/SkillDescription.java
+++ repositories/src/main/java/org/savageworlds/game/model/SkillDescription.java
@@ -3,12 +3,6 @@ package org.savageworlds.game.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 
 import jdo.model.BasePersistentModel;
@@ -18,7 +12,6 @@ import org.hibernate.validator.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @JsonRootName("skillDescription")
 public class SkillDescription extends BasePersistentModel implements Serializable{
 
