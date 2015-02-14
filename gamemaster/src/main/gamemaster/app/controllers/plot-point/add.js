ff6605ad38fff7a315dad3677f8f1f8172e713b3
Jim Barrows <Jim.Barrows@gmail.com>
2015-02-13 20:38:23 -0700

    Added flash messages

--- gamemaster/src/main/gamemaster/app/controllers/plot-point/add.js
+++ gamemaster/src/main/gamemaster/app/controllers/plot-point/add.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	actions: {
 		save:function() {
-			this.model.save();
+			var controller = this;
+			this.model.save().then(function() {
+				Ember.get(controller, 'flashes').success('Success!', 2000);
+				controller.transitionToRoute('plot-point.edit', controller.model);
+			});
 		},
 		cancel: function() {
 			this.model.destroyRecord();
--- gamemaster/src/main/gamemaster/app/controllers/plot-point/edit.js
+++ gamemaster/src/main/gamemaster/app/controllers/plot-point/edit.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	actions: {
-		save:function() {
-			this.model.save();
+		save:function() {			
+			var controller = this;
+			this.model.save().then(function() {
+				Ember.get(controller, 'flashes').success('Success!', 2000);				
+			});
 		},
 		cancel: function() {
 			this.model.rollback();
