6014c399361cca832e4eca0cba3d1bbfc4b8d8b4
Jim Barrows <Jim.Barrows@gmail.com>
2015-03-06 20:23:12 -0700

    Changed directory name on the server mock

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -9,7 +9,7 @@
 
 module.exports = function(app) {
   var globSync   = require('glob').sync;
-  var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
+  var mocks      = globSync('./routes/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
   // Log proxy requests
