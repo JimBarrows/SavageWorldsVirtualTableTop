6d0c18698a1c5de80116bcaf0ceaedc81743b804
Jim Barrows <Jim.Barrows@gmail.com>
2015-05-23 21:28:46 -0700

    Adding a description to the story.

--- SavageWorlds/client/storyForm.js
+++ SavageWorlds/client/storyForm.js
@@ -2,18 +2,20 @@ Template.StoryForm.events({
   "submit #storyForm": function (event) {
     // This function is called when the new task form is submitted
 
-    var title = event.target.title.value;
     var id = event.target.id.value;
+    var title = event.target.title.value;
+    var description = event.target.description.value;
+
+    var storyRecord = {
+      title: title,
+      owner: Meteor.userId(),
+      description: description
+    };
+
     if( id) {
-      Stories.update(id, {
-        title: title
-      });
-      
+      Stories.update(id, storyRecord);
     } else {
-      Stories.insert({
-        title: title,
-        owner: Meteor.userId(),
-      });
+      Stories.insert(storyRecord);
     }
 
     Router.go('story.list');
