f7b4dee8f8a8fe621b23250f45b135e02106b3a8
Jim Barrows <Jim.Barrows@gmail.com>
2015-08-15 12:26:44 -0700

    migrations

---
+++ backend/migrations/20150809130822-0-1-0.js
@@ -0,0 +1,26 @@
+'use strict';
+var Promise = require("bluebird");
+var join = Promise.join;
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
