0707102f68a2cba416dbd9b741d59563104330db
Jim Barrows <jim@reallybigtree.com>
2016-11-24 14:39:38 -0700

    Can post to create a plot point.

--- server/routes/PlotPoints.js
+++ server/routes/PlotPoints.js
@@ -14,13 +14,18 @@ router.get("/", isAuthenticated, function (req, res) {
 });
 
 router.post("/", isAuthenticated, function (req, res) {
-	let plotPoint     = req.body;
+	let plotPoint  = req.body;
 	plotPoint.user = req.user._id;
-	PlotPoint.create(plotPoint)
-			.then((plotPoint) => res.status(201).json(plotPoint).end())
-			.catch((error) => {
-				res.status(400).end();
-			});
+	if (plotPoint.name) {
+		PlotPoint.create(plotPoint)
+				.then((plotPoint) => res.status(201).json(plotPoint).end())
+				.catch((error) => {
+					res.status(400).json({error}).end();
+				});
+	} else {
+		res.status(400).json({"error": {"validation": {"name": "is required"}}}).end();
+	}
+
 });
 
 router.put("/:plotPointId", isAuthenticated, function (req, res) {
--- server/tests/plotpoint.spec.js
+++ server/tests/plotpoint.spec.js
@@ -1,4 +1,4 @@
-import {client, expect, Promise, plotPoint, startTestHouseKeeping} from "./support/fixtures";
+import {client, expect, Promise, plotPoint as createPlotPoint, startTestHouseKeeping} from "./support/fixtures";
 import PlotPoint from "../models/PlotPoint";
 
 describe("The plot points API", function () {
@@ -18,10 +18,10 @@ describe("The plot points API", function () {
 			let unownedPlotPoints = [];
 
 			beforeEach(() => Promise.all(
-					[plotPoint("first", currentUser._id),
-						plotPoint("second", currentUser._id),
-						plotPoint("first unowned"),
-						plotPoint("second  unowned")]
+					[createPlotPoint("first", currentUser._id),
+						createPlotPoint("second", currentUser._id),
+						createPlotPoint("first unowned"),
+						createPlotPoint("second  unowned")]
 							.map((pp) => PlotPoint.create(pp)))
 					.then((responses) => responses.forEach((pp) => pp.user ? plotPoints.push(pp) : unownedPlotPoints.push(pp)))
 			);
@@ -37,6 +37,34 @@ describe("The plot points API", function () {
 							expect(data).to.have.lengthOf(plotPoints.length);
 							expect(data.map((pp) => pp.name)).to.include.members(plotPoints.map((pp) => pp.name));
 							expect(data.map((pp) => pp.name)).to.not.include.members(unownedPlotPoints.map((pp) => pp.name));
+						});
+			});
+		});
+		describe("POST method", function () {
+
+			it("creates a valid plot point", function () {
+				let plotPoint = createPlotPoint("perfectly good plot point", currentUser._id);
+				return client.post(url, plotPoint)
+						.then((response) => {
+							expect(response.status).to.be.equal(201);
+							return response.data;
+						})
+						.then((data) => {
+							expect(data).to.exist;
+							expect(data._id).to.exist;
+							expect(data.name).to.be.equal(plotPoint.name);
+						})
+			});
+			it("rejects a plot point without a name.", function () {
+				let plotPoint = createPlotPoint("perfectly good plot point", currentUser._id);
+				delete plotPoint.name;
+				return client.post(url, plotPoint)
+						.then((response) => {
+							expect(response.status).to.be.equal(400);
+							return response.data;
+						})
+						.then((data) => {
+							expect(data).to.deep.equal({"error": {"validation": {"name": "is required"}}});
 						});
 			});
 		});
