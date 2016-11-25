34b95ec453e8affaddb373e9b8035859548d0934
Jim Barrows <jim@reallybigtree.com>
2016-11-24 23:38:33 -0700

    Delete works

--- server/tests/plotpoint.spec.js
+++ server/tests/plotpoint.spec.js
@@ -3,7 +3,7 @@ import PlotPoint from "../models/PlotPoint";
 
 describe("/api/plotpoint/:plotPointId", function () {
 
-	let url      = "/api/plotpoint";
+	let url      = "";
 	let currentUser = {};
 	let original = {};
 	let updated  = {};
@@ -11,13 +11,14 @@ describe("/api/plotpoint/:plotPointId", function () {
 	beforeEach(() => startTestHouseKeeping()
 			.then((account) => currentUser = account)
 			.then(() => PlotPoint.create(createPlotPoint("original plot point", currentUser._id)))
-			.then((created) => original = created));
+			.then((created) => original = created)
+			.then(() => url = `/api/plotpoint/${original._id}`));
 
 	describe("PUT method", function () {
 
 		it("should update a plot point", function () {
 			updated = Object.assign(original, {name: "this is a new name"});
-			return client.put(`${url}/${updated._id}`, updated)
+			return client.put(url, updated)
 					.then((response) => {
 						expect(response.status).to.be.equal(200);
 						return PlotPoint.findById(updated._id).exec();
@@ -29,12 +30,21 @@ describe("/api/plotpoint/:plotPointId", function () {
 
 		it("should reject a plot point update with no name", function () {
 			updated = Object.assign(original, {name: ""});
-			return client.put(`${url}/${updated._id}`, updated)
+			return client.put(url, updated)
 					.then((response) => {
 						expect(response.status).to.be.equal(400);
 
 					});
 		});
 	});
-	// describe("");
+	describe("DELETE method", function () {
+		it("should delete a plot point", function () {
+			return client.delete(url)
+					.then((response) => {
+						expect(response.status).to.be.equal(200);
+					})
+					.then(() => PlotPoint.findById(original._id))
+					.then((plotPoint) => expect(plotPoint).to.not.exist);
+		});
+	});
 });
