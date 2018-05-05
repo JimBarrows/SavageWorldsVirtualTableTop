a6e91b7c9ecf77442c320b9598dccd713cb58dc3
Jim Barrows <JIm.Barrows@gmail.com>
2018-05-05 17:08:40 -0500

    Can get to the add page.

--- ui-web/src/pages/PlotPointEditor.jsx
+++ ui-web/src/pages/PlotPointEditor.jsx
@@ -1,15 +1,7 @@
+import {API} from 'aws-amplify';
 import {PageHeader} from 'bootstrap-react-components';
 import React from 'react';
-import {connect} from 'react-redux';
-import {
-	addRace,
-	addRacialAbility,
-	cancelChanges,
-	descriptionChange, loadPlotPoint,
-	maximumAttributePointsChange,
-	maximumMajorHindrancesChange, maximumMinorHindrancesChange,
-	maximumSkillPointsChange, nameChange, newPlotPoint, raceChange, savePlotPoint
-} from '../actions';
+import {withRouter} from 'react-router';
 import NumberFormGroup from '../components/NumberFormGroup';
 import {RaceEditor} from '../components/Race';
 import TextAreaFormGroup from '../components/TextAreaFormGroup';
@@ -22,71 +14,79 @@ class PlotPointEditor extends React.Component {
 		id: 'PlotPointEditorPage'
 	};
 
+	state = {
+		name                  : '',
+		description           : '',
+		maximumAttributePoints: 5,
+		maximumMajorHindrances: 1,
+		maximumMinorHindrances: 2,
+		maximumSkillPoints    : 15,
+		races                 : []
+	};
+
 	addRace = (event) => {
 		event.preventDefault();
 		this.props.addRace();
 	};
 
-	addRacialAbility = (indexOfRace) => this.props.addRacialAbility(indexOfRace);
-
-	componentDidMount() {
-		if (this.props.match.params.name) {
-			this.props.loadPlotPoint(this.props.match.params.name);
-		} else {
-			this.props.newPlotPoint();
-		}
-	}
+	addRacialAbility             = (indexOfRace) => this.props.addRacialAbility(indexOfRace);
+	maximumAttributePointsChange = e => this.setState({maximumAttributePoints: parseInt(e.target.value, 10)});
 
 	cancel = e => {
 		e.preventDefault();
 		this.props.cancel();
 	};
 
-	deleteRacialAbility = (raceIndex, racialAbilityIndex) => {
-		this.props.deleteRacialAbility(raceIndex, racialAbilityIndex);
+	descriptionChange            = e => this.props.descriptionChange(e.target.value);
+	maximumMajorHindrancesChange = e => this.setState({maximumMajorHindrances: parseInt(e.target.value, 10)});
+	maximumMinorHindrancesChange = e => this.setState({maximumMinorHindrances: parseInt(e.target.value, 10)});
+	maximumSkillPointsChange     = e => this.setState({maximumSkillPoints: parseInt(e.target.value, 10)});
+	nameChange                   = e => this.setState({name: e.target.value});
+	raceChange                   = (race, index) => this.setState({races: this.state.races.map((r, i) => i === index ? race : r)});
+	races                        = () => {
+		if (this.state.races.length === 0) {
+			return <p>No races</p>;
+		} else {
+			return this.state.races.map((race, index) =>
+					<RaceEditor key={index} index={index} race={race}
+					            addRacialAbility={this.addRacialAbility}
+					            deleteRacialAbility={this.deleteRacialAbility}
+					            onChange={this.raceChange}/>);
+		}
 	};
 
-	descriptionChange = e => this.props.descriptionChange(e.target.value);
-
-	maximumAttributePointsChange = e => this.props.maximumAttributePointsChange(e.target.value);
-
-	maximumMajorHindrancesChange = e => this.props.maximumMajorHindrancesChange(e.target.value);
-
-	maximumMinorHindrancesChange = e => this.props.maximumMinorHindrancesChange(e.target.value);
-
-	maximumSkillPointsChange = e => this.props.maximumSkillPointsChange(e.target.value);
-
-	nameChange = e => this.props.nameChange(e.target.value);
-
-	raceChange = (race, index) => this.props.raceChange(race, index);
+	async componentDidMount() {
+		if (this.props.match.params.name) {
+			let plotPoint = await API.get('PlotPointsCRUD', `/plotPoints/object/${this.props.match.params.name}`);
+			this.setState({
+				...plotPoint
+			});
+		}
+	}
 
 	render() {
 		return <div id={this.props.id}>
-			<PageHeader id={this.props.id}><h1>Plotpoint Editor</h1></PageHeader>
+			<PageHeader id={this.props.id}><h1>Plot Point Editor</h1></PageHeader>
 			<form id='plotPointForm'>
 				<TextFormGroup id='plotPointName' label='Name' onChange={this.nameChange} required={true}
-				               value={this.props.name}/>
+				               value={this.state.name}/>
 				<TextAreaFormGroup id={'plotPointDescription'} label={'Description'} onChange={this.descriptionChange}
-				                   value={this.props.description}/>
+				                   value={this.state.description}/>
 				<NumberFormGroup id={'maximumAttributePoints'} label={'Maximum Attribute Points'}
 				                 onChange={this.maximumAttributePointsChange} required={true}
-				                 value={this.props.maximumAttributePoints}/>
+				                 value={this.state.maximumAttributePoints}/>
 				<NumberFormGroup id={'maximumMajorHindrances'} label={'Maximum Number of Major Hindrances'}
 				                 onChange={this.maximumMajorHindrancesChange} required={true}
-				                 value={this.props.maximumMajorHindrances}/>
+				                 value={this.state.maximumMajorHindrances}/>
 				<NumberFormGroup id={'maximumMinorHindrances'} label={'Maximum Number of Minor Hindrances'}
 				                 onChange={this.maximumMinorHindrancesChange} required={true}
-				                 value={this.props.maximumMinorHindrances}/>
+				                 value={this.state.maximumMinorHindrances}/>
 				<NumberFormGroup id={'maximumSkillPoints'} label={'Maximum Skill Points'}
 				                 onChange={this.maximumSkillPointsChange} required={true}
-				                 value={this.props.maximumSkillPoints}/>
+				                 value={this.state.maximumSkillPoints}/>
 				<h2>Races</h2>
 				<button id={'addRaceButton'} className="btn btn-default" onClick={this.addRace}>Add</button>
-				{this.props.races.map((race, index) =>
-						<RaceEditor key={index} index={index} race={race}
-						            addRacialAbility={this.addRacialAbility}
-						            deleteRacialAbility={this.deleteRacialAbility}
-						            onChange={this.raceChange}/>)}
+				{this.races()}
 				<button id={'savePlotPointButton'} type={'submit'} className={'btn btn-default'} onClick={this.save}>Save
 				</button>
 				<button id={'cancelPlotPointButton'} type={'cancel'} className={'btn btn-default'}
@@ -102,35 +102,4 @@ class PlotPointEditor extends React.Component {
 	};
 }
 
-const mapStateToProps = (state) => {
-	return {
-
-		description           : state.PlotPoint.description,
-		maximumAttributePoints: state.PlotPoint.maximumAttributePoints,
-		maximumMajorHindrances: state.PlotPoint.maximumMajorHindrances,
-		maximumMinorHindrances: state.PlotPoint.maximumMinorHindrances,
-		maximumSkillPoints    : state.PlotPoint.maximumSkillPoints,
-		name                  : state.PlotPoint.name,
-		races                 : state.PlotPoint.races
-	};
-};
-
-const mapDispatchToProps = (dispatch) => {
-	return {
-		addRace                     : () => dispatch(addRace()),
-		addRacialAbility            : (indexOfRace) => dispatch(addRacialAbility(indexOfRace)),
-		cancel                      : () => dispatch(cancelChanges()),
-		descriptionChange           : (description) => dispatch(descriptionChange(description)),
-		loadPlotPoint               : (name) => dispatch(loadPlotPoint(name)),
-		maximumAttributePointsChange: (maximumAttributePoints) => dispatch(maximumAttributePointsChange(maximumAttributePoints)),
-		maximumMajorHindrancesChange: (maximumMajorHindrances) => dispatch(maximumMajorHindrancesChange(maximumMajorHindrances)),
-		maximumMinorHindrancesChange: (maximumMinorHindrances) => dispatch(maximumMinorHindrancesChange(maximumMinorHindrances)),
-		maximumSkillPointsChange    : (maximumSkillPoints) => dispatch(maximumSkillPointsChange(maximumSkillPoints)),
-		nameChange                  : (name) => dispatch(nameChange(name)),
-		newPlotPoint                : () => dispatch(newPlotPoint()),
-		raceChange                  : (race, index) => dispatch(raceChange(race, index)),
-		savePlotPoint               : () => dispatch(savePlotPoint())
-	};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(PlotPointEditor);
+export default withRouter(PlotPointEditor);
