67c2a7c42da928f0cab29a8621a897297d3c1524
Jim Barrows <JIm.Barrows@gmail.com>
2018-06-28 08:28:30 -0500

    More storybook changes.

--- ui-web/stories/BaseVehicleEditor.stories.js
+++ ui-web/stories/BaseVehicleEditor.stories.js
@@ -1,8 +1,11 @@
+import {action} from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react'
-
 import React from 'react'
 import BaseVehicleEditor from '../src/components/BaseVehicleEditor'
 
-storiesOf('Base Vehicle Editor', module)
-  .add('Default', () => <BaseVehicleEditor/>)
+storiesOf('Editors/Base Vehicle', module)
+  .addDecorator((story) => <div className="container">
+    <form>{story()}</form>
+  </div>)
+  .add('Default', () => <BaseVehicleEditor item={({name: ''})} onChange={action("Changed")}/>)
 
