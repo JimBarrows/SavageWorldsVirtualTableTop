48114655c6a04d9f53e0c471e4fa3ecc7e8fc2c1
Jim Barrows <JIm.Barrows@gmail.com>
2018-08-13 08:35:21 -0500

    [#99] Beast skills - When adding a new one, and not adding a skill delete does not work
    Fixes[#99] Splice does not return the modified array, but rather the changes to the array.  Also replace html button with bootstrap-react-components button.

--- ui-web/src/components/lists/SelectedSkillList.jsx
+++ ui-web/src/components/lists/SelectedSkillList.jsx
@@ -1,46 +1,48 @@
+import {Button} from 'bootstrap-react-components'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SelectedSkillEditor from '../editors/SelectedSkillEditor'
 
 export default class SelectedSkillList extends React.Component {
 
-  static propTypes = {
-    id             : PropTypes.string.isRequired,
-    skillsAvailable: PropTypes.array.isRequired,
-    skills         : PropTypes.array.isRequired,
-    onChange       : PropTypes.func.isRequired
-  }
+	static propTypes = {
+		id             : PropTypes.string.isRequired,
+		skillsAvailable: PropTypes.array.isRequired,
+		skills         : PropTypes.array.isRequired,
+		onChange       : PropTypes.func.isRequired
+	}
 
-  static defaultProps = {}
+	static defaultProps = {}
 
-  addSkill = (e) => {
-    e.preventDefault()
-    this.props.onChange([{name: ' ', rank: {dice: 'd4', bonus: 0}, note: ' '}, ...this.props.skills])
-  }
+	addSkill = (e) => {
+		e.preventDefault()
+		this.props.onChange([{name: ' ', rank: {dice: 'd4', bonus: 0}, note: ' '}, ...this.props.skills])
+	}
 
-  skillChanged = (indexOfChange, changedSkill) => {
-    this.props.onChange(this.props.skills.map((skill, index) => indexOfChange === index ? changedSkill : skill))
-  }
+	skillChanged = (indexOfChange, changedSkill) => {
+		this.props.onChange(this.props.skills.map((skill, index) => indexOfChange === index ? changedSkill : skill))
+	}
 
-  skillDeleted = indexOfSkill => this.props.onChange(this.props.skills.splice(indexOfSkill, 1))
+	skillDeleted = indexOfSkill => {
+		this.props.skills.splice(indexOfSkill, 1)
+		this.props.onChange(this.props.skills)
+	}
 
-  skillEditorList = () => this.props.skills.map((selectedSkill, index) => <SelectedSkillEditor key={index}
-                                                                                               id={'skill_' + index}
-                                                                                               index={index}
-                                                                                               onChange={this.skillChanged}
-                                                                                               onDelete={this.skillDeleted}
-                                                                                               skill={selectedSkill}
-                                                                                               skillsAvailable={this.props.skillsAvailable}/>)
+	skillEditorList = () => this.props.skills.map((selectedSkill, index) => <SelectedSkillEditor key={index}
+	                                                                                             id={'skill_' + index}
+	                                                                                             index={index}
+	                                                                                             onChange={this.skillChanged}
+	                                                                                             onDelete={this.skillDeleted}
+	                                                                                             skill={selectedSkill}
+	                                                                                             skillsAvailable={this.props.skillsAvailable}/>)
 
-  render() {
-    return (
-      <div id={'SelectedSkillListComponent_' + this.props.id}>
-        <h3>Skills</h3>
-        <button id={`addSelectedSkillButton+_${this.props.id}`} className="btn btn-default"
-                onClick={this.addSkill}>Add
-        </button>
-        {this.skillEditorList()}
-      </div>
-    )
-  }
+	render() {
+		let componentId = `SelectedSkillList-${this.props.id}`
+
+		return <div id={componentId}>
+			<h3>Skills</h3>
+			<Button id={`${componentId}-AddButton`} onClick={this.addSkill}>Add</Button>
+			{this.skillEditorList()}
+		</div>
+	}
 }
