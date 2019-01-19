180f0e0c9df5fb74d22e06bce92994c25df5832f
Jim Barrows <Jim.Barrows@gmail.com>
2019-01-19 17:44:06 -0600

    Starting to get everything working again.

--- ui-web/src/components/PlotPointEditor/Navigation.jsx
+++ ui-web/src/components/PlotPointEditor/Navigation.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import ALink from '../ALink'
+import React     from 'react'
+import Nav       from 'bootstrap-react-components/bootstrap/components/Nav'
+import NavItem   from 'bootstrap-react-components/bootstrap/components/Nav/NavItem'
 
 export default class Navigation extends React.Component {
 
@@ -13,145 +14,147 @@ export default class Navigation extends React.Component {
 
 	navigateTo = name => this.props.navigateTo(name)
 
-	render() {
+	render () {
 		let {id}         = this.props
 		let component_id = `Navigation-${id}`
-		return (
-			<nav id={component_id} className="col-md-2 d-none d-md-block bg-light sidebar">
-				<div className="sidebar-sticky">
-					<ul className="nav flex-column">
-						<li className="nav-item">
-							<ALink id={component_id + 'PlotPoint'} className="nav-link active"
-							       onClick={e => {this.navigateTo('PlotPoint')}}>
-								Plot Point <span className="sr-only">(current)</span>
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'SettingRules'} className="nav-link"
-							       onClick={e => this.navigateTo('SettingRules')}>
-								Setting Rules
-							</ALink>
-						</li>
-						<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-							<span>Character Creation</span>
-						</h6>
-						<li className="nav-item">
-							<ALink id={component_id + 'Races'} className="nav-link" onClick={e => this.navigateTo('Races')}>
-								Races
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Skills'} className="nav-link" onClick={e => this.navigateTo('Skills')}>
-								Skills
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Hindrances'} className="nav-link" onClick={e => this.navigateTo('Hindrances')}>
-								Hindrances
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Edges'} className="nav-link" onClick={e => this.navigateTo('Edges')}>
-								Edges
-							</ALink>
-						</li>
-						<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-							<span>Gear</span>
-						</h6>
-						<li className="nav-item">
-							<ALink id={component_id + 'MundaneItems'} className="nav-link"
-							       onClick={e => this.navigateTo('MundaneItems')}>
-								Mundane Items
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'HandWeapons'} className="nav-link"
-							       onClick={e => this.navigateTo('HandWeapons')}>
-								Hand Weapons
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Armor'} className="nav-link" onClick={e => this.navigateTo('Armor')}>
-								Armor
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Armor'} className="nav-link" onClick={e => this.navigateTo('RangedWeapons')}>
-								Ranged Weapons
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'VehicleMountedAndAtGuns'} className="nav-link"
-							       onClick={e => this.navigateTo('VehicleMountedAndAtGuns')}>
-								Vehicle Mounted & AT Guns
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Ammunition'} className="nav-link" onClick={e => this.navigateTo('Ammunition')}>
-								Ammunition
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Index'} className="nav-link"
-							       onClick={e => this.navigateTo('Index')}>
-								Special Weapons
-							</ALink>
-						</li>
-						<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-							<span>Vehicles</span>
-						</h6>
-						<li className="nav-item">
-							<ALink id={component_id + 'GroundVehicle'} className="nav-link"
-							       onClick={e => this.navigateTo('GroundVehicles')}>
-								Ground
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'WaterVehicle'} className="nav-link"
-							       onClick={e => this.navigateTo('WaterVehicles')}>
-								Water
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Aircraft'} className="nav-link" onClick={e => this.navigateTo('Aircraft')}>
-								Air
-							</ALink>
-						</li>
-						<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-							<span>Powers</span>
-						</h6>
-						<li className="nav-item">
-							<ALink id={component_id + 'ArcaneBackground'} className="nav-link"
-							       onClick={e => this.navigateTo('ArcaneBackground')}>
-								Arcane Backgrounds
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Trapping'} className="nav-link"
-							       onClick={e => this.navigateTo('TrappingsAndEffects')}>
-								Trappings & Effects
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Powers'} className="nav-link" onClick={e => this.navigateTo('Powers')}>
-								Powers
-							</ALink>
-						</li>
-						<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-							<span>Allies, Critters & Enemies</span></h6>
-						<li className="nav-item">
-							<ALink id={component_id + 'Beasts'} className="nav-link" onClick={e => this.navigateTo('Beasts')}>
-								Beasts
-							</ALink>
-						</li>
-						<li className="nav-item">
-							<ALink id={component_id + 'Characters'} className="nav-link" onClick={e => this.navigateTo('Characters')}>
-								Characters
-							</ALink>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		)
+		return (<Nav id={component_id} look={'tabs'} >
+			<NavItem id={component_id + '-basic-rules'} onClick={this.navigateTo('PlotPoint')} label={'Basic Rules'}/>
+		</Nav >)
+		// <nav id={component_id} className="col-md-2 d-none d-md-block bg-light sidebar">
+		// 	<div className="sidebar-sticky">
+		// 		<ul className="nav flex-column">
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'PlotPoint'} className="nav-link active"
+		// 				       onClick={e => {this.navigateTo('PlotPoint')}}>
+		// 					Plot Point <span className="sr-only">(current)</span>
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'SettingRules'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('SettingRules')}>
+		// 					Setting Rules
+		// 				</ALink>
+		// 			</li>
+		// 			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+		// 				<span>Character Creation</span>
+		// 			</h6>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Races'} className="nav-link" onClick={e => this.navigateTo('Races')}>
+		// 					Races
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Skills'} className="nav-link" onClick={e => this.navigateTo('Skills')}>
+		// 					Skills
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Hindrances'} className="nav-link" onClick={e => this.navigateTo('Hindrances')}>
+		// 					Hindrances
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Edges'} className="nav-link" onClick={e => this.navigateTo('Edges')}>
+		// 					Edges
+		// 				</ALink>
+		// 			</li>
+		// 			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+		// 				<span>Gear</span>
+		// 			</h6>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'MundaneItems'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('MundaneItems')}>
+		// 					Mundane Items
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'HandWeapons'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('HandWeapons')}>
+		// 					Hand Weapons
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Armor'} className="nav-link" onClick={e => this.navigateTo('Armor')}>
+		// 					Armor
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Armor'} className="nav-link" onClick={e => this.navigateTo('RangedWeapons')}>
+		// 					Ranged Weapons
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'VehicleMountedAndAtGuns'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('VehicleMountedAndAtGuns')}>
+		// 					Vehicle Mounted & AT Guns
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Ammunition'} className="nav-link" onClick={e => this.navigateTo('Ammunition')}>
+		// 					Ammunition
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Index'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('Index')}>
+		// 					Special Weapons
+		// 				</ALink>
+		// 			</li>
+		// 			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+		// 				<span>Vehicles</span>
+		// 			</h6>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'GroundVehicle'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('GroundVehicles')}>
+		// 					Ground
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'WaterVehicle'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('WaterVehicles')}>
+		// 					Water
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Aircraft'} className="nav-link" onClick={e => this.navigateTo('Aircraft')}>
+		// 					Air
+		// 				</ALink>
+		// 			</li>
+		// 			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+		// 				<span>Powers</span>
+		// 			</h6>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'ArcaneBackground'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('ArcaneBackground')}>
+		// 					Arcane Backgrounds
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Trapping'} className="nav-link"
+		// 				       onClick={e => this.navigateTo('TrappingsAndEffects')}>
+		// 					Trappings & Effects
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Powers'} className="nav-link" onClick={e => this.navigateTo('Powers')}>
+		// 					Powers
+		// 				</ALink>
+		// 			</li>
+		// 			<h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+		// 				<span>Allies, Critters & Enemies</span></h6>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Beasts'} className="nav-link" onClick={e => this.navigateTo('Beasts')}>
+		// 					Beasts
+		// 				</ALink>
+		// 			</li>
+		// 			<li className="nav-item">
+		// 				<ALink id={component_id + 'Characters'} className="nav-link" onClick={e => this.navigateTo('Characters')}>
+		// 					Characters
+		// 				</ALink>
+		// 			</li>
+		// 		</ul>
+		// 	</div>
+		// </nav>
+
 	}
 }
--- ui-web/src/components/PlotPointEditor/PlotPoint.jsx
+++ ui-web/src/components/PlotPointEditor/PlotPoint.jsx
@@ -1,6 +1,7 @@
 import {NumberFormGroup, TextAreaFormGroup, TextFormGroup} from 'bootstrap-react-components'
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes                                           from 'prop-types'
+import React                                               from 'react'
+import Navigation                                          from './Navigation'
 
 export default class PlotPoint extends React.Component {
 
@@ -18,30 +19,33 @@ export default class PlotPoint extends React.Component {
 	maximumMinorHindrancesChange = e => this.props.onChange(Object.assign({}, this.props.plotPoint, {maximumMinorHindrances: parseInt(e.target.value, 10)}))
 	maximumSkillPointsChange     = e => this.props.onChange(Object.assign({}, this.props.plotPoint, {maximumSkillPoints: parseInt(e.target.value, 10)}))
 	nameChange                   = e => this.props.onChange(Object.assign({}, this.props.plotPoint, {name: e.target.value}))
+	showSection                  = section => console.log('section: ', section)
 
-	render() {
+	render () {
 		let {id, plotPoint} = this.props
 		let componentId     = `PlotPoint-${id}`
 		return (
-			<div id={componentId}>
+			<div id={componentId} >
 				<TextFormGroup id={`${componentId}-Name`} label='Name' onChange={this.nameChange} required={true}
-				               value={plotPoint.name}/>
+					value={plotPoint.name} />
 				<TextAreaFormGroup id={`${componentId}-Description`} label={'Description'} onChange={this.descriptionChange}
-				                   value={plotPoint.description}/>
-				<h1>Basic Rules</h1>
+					value={plotPoint.description} />
+				<Navigation id={componentId} navigateTo={this.showSection} />
+				<h1 >Basic Rules</h1 >
 				<NumberFormGroup id={`${componentId}-MaximumAttributePoints`} label={'Maximum Attribute Points'}
-				                 onChange={this.maximumAttributePointsChange} required={true}
-				                 value={plotPoint.maximumAttributePoints}/>
+					onChange={this.maximumAttributePointsChange} required={true}
+					value={plotPoint.maximumAttributePoints} />
 				<NumberFormGroup id={`${componentId}-MaximumMajorHindrances`} label={'Maximum Number of Major Hindrances'}
-				                 onChange={this.maximumMajorHindrancesChange} required={true}
-				                 value={plotPoint.maximumMajorHindrances}/>
+					onChange={this.maximumMajorHindrancesChange} required={true}
+					value={plotPoint.maximumMajorHindrances} />
 				<NumberFormGroup id={`${componentId}-MaximumMinorHindrances`} label={'Maximum Number of Minor Hindrances'}
-				                 onChange={this.maximumMinorHindrancesChange} required={true}
-				                 value={plotPoint.maximumMinorHindrances}/>
+					onChange={this.maximumMinorHindrancesChange} required={true}
+					value={plotPoint.maximumMinorHindrances} />
 				<NumberFormGroup id={`${componentId}-MaximumSkillPoints`} label={'Maximum Skill Points'}
-				                 onChange={this.maximumSkillPointsChange} required={true}
-				                 value={plotPoint.maximumSkillPoints}/>
-			</div>
+					onChange={this.maximumSkillPointsChange} required={true}
+					value={plotPoint.maximumSkillPoints} />
+
+			</div >
 		)
 	}
 }
