7c82cdfc601b6648594200e974cfc22ee70804d1
Jim Barrows <Jim.Barrows@gmail.com>
2024-02-09 09:52:41 -0600

    Changes

--- ui-web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ ui-web/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
-	"api": {
-		"swvtt": {
-			"GraphQLAPIKeyOutput": "string",
-			"GraphQLAPIIdOutput": "string",
-			"GraphQLAPIEndpointOutput": "string"
-		}
-	},
-	"auth": {
-		"swvtt9e8d353f": {
-			"IdentityPoolId": "string",
-			"IdentityPoolName": "string",
-			"UserPoolId": "string",
-			"UserPoolArn": "string",
-			"UserPoolName": "string",
-			"AppClientIDWeb": "string",
-			"AppClientID": "string"
-		}
-	}
+    "api": {
+        "swvtt": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "swvtt9e8d353f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
 }
